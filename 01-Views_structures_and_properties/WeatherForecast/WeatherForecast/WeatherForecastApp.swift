af3e057e8954b72d7de7bcca062f6c431664d13e
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-05 17:56:35 +0200

    • e Create WeatherForecast project

---
+++ 01-Views_structures_and_properties/WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Ignasi Perez-Valls on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
