6f5d85fd59af62be5791e29496ce131ce6c74548
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-05 18:33:18 +0200

    • r Create a custom subview

--- 01-Views_structures_and_properties/WeatherForecast/WeatherForecast/ContentView.swift
+++ 01-Views_structures_and_properties/WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,26 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            VStack {
-                Text("Mon")
-                Image(systemName: "sun.max.fill")
-                    .foregroundStyle(Color.yellow)
-                Text("High: 70")
-                Text("Low: 50")
-            }
-            .padding()
-            VStack {
-                Text("Tue")
-                Image(systemName: "cloud.rain.fill")
-                    .foregroundStyle(Color.blue)
-                Text("High: 60")
-                Text("Low: 40")
-            }
-            .padding()
+            DayForecast()
+            
+            DayForecast()
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    var body: some View {
+        VStack {
+            Text("Mon")
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("High: 70")
+            Text("Low: 50")
+        }
+        .padding()
+    }
 }
