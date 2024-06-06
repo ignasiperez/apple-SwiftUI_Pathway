72add5c7f17268b5103fc2e33afabd3e26e2048c
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-06 17:40:28 +0200

    • f Create HomeScreen view

---
+++ 01-Views_structures_and_properties/AboutMe/AboutMe/Screens/HomeScreen.swift
@@ -0,0 +1,32 @@
+//
+//  HomeScreen.swift
+//  AboutMe
+//
+//  Created by Ignasi Perez-Valls on 6/6/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+        }
+        
+        Image(information.image)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(10)
+            .padding(40)
+        
+        Text(information.name)
+            .font(.title)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
