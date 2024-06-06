5024cf55f6ebbbd1a7262a7b228d7c7bd5540d0a
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-06 13:18:34 +0200

    • e Create AboutMe project

---
+++ 01-Views_structures_and_properties/AboutMe/AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Ignasi Perez-Valls on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
