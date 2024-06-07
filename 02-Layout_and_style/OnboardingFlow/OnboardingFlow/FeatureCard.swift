795eb67af8af369a0d40aca47f158b6a6afe98b0
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 18:44:39 +0200

    • f Create FeatureCard view basic skeleton

---
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,20 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by Ignasi Perez-Valls on 7/6/24.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text("")
+    }
+    
+} // FeatureCard
+
+
+#Preview {
+    FeatureCard()
+}
--- 02-Layout_and_style/OnboardingFlow/OnboardingFlow/FeaturesPage.swift
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/FeaturesPage.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct FeaturesPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+            
+        } // VStack
     }
     
 } // FeaturesPage
