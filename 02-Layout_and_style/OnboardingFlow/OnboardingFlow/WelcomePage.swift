91f4dd85369aa51fdc3e106580b75894557a1262
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 13:44:42 +0200

    • f Add an SF Symbol on top of the rounded rectangle using a ZStack

--- 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
@@ -10,23 +10,29 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 150, height: 150)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 150, height: 150)
                 .foregroundStyle(.tint)
+                
+                Image(systemName: "figure.2.and.child.holdinghands")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.white)
+            }
             
             Text("Welcome to MyApp")
                 .font(.title)
                 .fontWeight(.semibold)
                 .padding(.top)
-                .border(.black, width: 1.5)
+//              .border(.black, width: 1.5)
             
             Text("This app shows how to display an onboarding flow")
                 .font(.title2)
-                .border(.black, width: 1.5)
+//                .border(.black, width: 1.5)
         } // VStack
-        .border(.orange, width: 1.5)
+//      .border(.orange, width: 1.5)
         .padding()
-        .border(.purple, width: 1.5)
+//      .border(.purple, width: 1.5)
         
     }
 } // WelcomePage
