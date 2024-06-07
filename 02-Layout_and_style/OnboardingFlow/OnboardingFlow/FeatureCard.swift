dfdf6ff2dd35d76c97cb5565a3bf93174cd38de3
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 18:59:57 +0200

    • f Implement FeatureCard

--- 02-Layout_and_style/OnboardingFlow/OnboardingFlow/FeatureCard.swift
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/FeatureCard.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
     var body: some View {
-        Text("")
+        HStack {
+            Image(systemName: iconName)
+                .font(.largeTitle)
+            
+            Text(description)
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+        
     }
     
 } // FeatureCard
 
 
 #Preview {
-    FeatureCard()
+    FeatureCard(
+        iconName: "person.2.crop.square.stack.fill",
+        description: "A multiline description about a feature paired with the image on the left."
+    )
 }
