61d4d31c3097fce6ce74a336565b9f67264a61be
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 12:43:35 +0200

    • f Create WelcomePage view basic skeleton

---
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Ignasi Perez-Valls on 7/6/24.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
