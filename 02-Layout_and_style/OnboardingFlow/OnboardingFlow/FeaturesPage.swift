eab517eed55512e9836a8c8af5305f2b514dc551
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 13:49:53 +0200

    • f Create FeaturesPage view basic skeleton

---
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/FeaturesPage.swift
@@ -0,0 +1,20 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by Ignasi Perez-Valls on 7/6/24.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+} // FeaturesPage
+
+
+#Preview {
+    FeaturesPage()
+}
