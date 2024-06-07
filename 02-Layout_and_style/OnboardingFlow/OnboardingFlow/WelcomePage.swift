4cffdb775947ea96136541e8a31e8041110364c0
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 13:32:03 +0200

    • f Inspect the size of the views using borders

--- 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text("Welcome to MyApp")
-            .font(.title)
-            .fontWeight(.semibold)
+        VStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 150, height: 150)
+                .foregroundStyle(.tint)
+            
+            Text("Welcome to MyApp")
+                .font(.title)
+                .fontWeight(.semibold)
+                .border(.black, width: 1.5)
+            
+            Text("This app shows how to display an onboarding flow")
+                .font(.title2)
+                .border(.black, width: 1.5)
+        } // VStack
+        .border(.orange, width: 1.5)
+        .padding()
+        .border(.purple, width: 1.5)
         
     }
 } // WelcomePage
