e43bcd65e28377f56a4585b2eea272605c050139
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 12:36:21 +0200

    • e Create OnboardingFlow project

---
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnboardingFlow
+//
+//  Created by Ignasi Perez-Valls on 7/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Ignasi Perez-Valls on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
