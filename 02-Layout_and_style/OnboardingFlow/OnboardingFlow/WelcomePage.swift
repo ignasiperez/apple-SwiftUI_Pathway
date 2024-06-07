7a5136b4b782c9d34e30d8998bb9d7762459fcee
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-07 13:12:14 +0200

    • f Add "Welcome to MyApp" text

--- 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/WelcomePage.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Welcome to MyApp")
+            .font(.title)
+            .fontWeight(.semibold)
+        
     }
-}
+} // WelcomePage
+
 
 #Preview {
     WelcomePage()
