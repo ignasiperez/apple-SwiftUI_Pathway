50dce8648a093b7ff26ef0f6f096d7fa0c9db28d
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-08 08:01:45 +0200

    • f Add a gradient background to the tab view

--- 02-Layout_and_style/OnboardingFlow/OnboardingFlow/ContentView.swift
+++ 02-Layout_and_style/OnboardingFlow/OnboardingFlow/ContentView.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
+
 struct ContentView: View {
     var body: some View {
         TabView {
             WelcomePage()
             FeaturesPage()
         }
+        .background(Gradient(colors: gradientColors))
         .tabViewStyle(.page)
         
     }
