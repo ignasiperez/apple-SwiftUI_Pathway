98bbdca2f622071d1aea1ee7462f48fd1efaddf0
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-09 08:54:08 +0200

    • f Create LayingOutContainersView basic skeleton

---
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/Challenges/LayingOutContainersView.swift
@@ -0,0 +1,18 @@
+//
+//  LayingOutContainersView.swift
+//  LayingOutViews
+//
+//  Created by Ignasi Perez-Valls on 9/6/24.
+//
+
+import SwiftUI
+
+struct LayingOutContainersView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    LayingOutContainersView()
+}
