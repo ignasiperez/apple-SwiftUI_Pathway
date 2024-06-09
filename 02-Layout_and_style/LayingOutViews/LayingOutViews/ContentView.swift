f8996d3a77e4c2fd57d31c53e71ee7d23d2eed67
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-09 08:30:16 +0200

    • e Create LayingOutViews project

---
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayingOutViews
+//
+//  Created by Ignasi Perez-Valls on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/LayingOutViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayingOutViewsApp.swift
+//  LayingOutViews
+//
+//  Created by Ignasi Perez-Valls on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LayingOutViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
