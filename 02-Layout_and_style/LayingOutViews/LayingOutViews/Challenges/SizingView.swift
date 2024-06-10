5fc22f12b689f952fe900556c30e0a94f5b371f6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-10 12:10:28 +0200

    • f Create SizingView basic skeleton

---
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/Challenges/SizingView.swift
@@ -0,0 +1,18 @@
+//
+//  SizingView.swift
+//  LayingOutViews
+//
+//  Created by Ignasi Perez-Valls on 10/6/24.
+//
+
+import SwiftUI
+
+struct SizingView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    SizingView()
+}
