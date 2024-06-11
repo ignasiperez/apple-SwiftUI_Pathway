e6b0060d1ab866801deb1235f313961866764882
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-11 13:30:06 +0200

    • f Create MoodViewFull basic skeleton

---
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/Cards/MoodViewFull.swift
@@ -0,0 +1,19 @@
+//
+//  MoodViewFull.swift
+//  LayingOutViews
+//
+//  Created by Ignasi Perez-Valls on 11/6/24.
+//
+
+import SwiftUI
+
+struct MoodViewFull: View {
+    var body: some View {
+        Text("")
+        
+    }
+} // MoodViewFull
+
+#Preview {
+    MoodViewFull()
+}
