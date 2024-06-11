5802bc8bd287584ebdcefa9af0a43602f23d6a28
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-11 12:09:51 +0200

    • f Create DebuggingViews basic skeleton

---
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/Challenges/DebuggingViews.swift
@@ -0,0 +1,28 @@
+//
+//  DebuggingViews.swift
+//  LayingOutViews
+//
+//  Created by Ignasi Perez-Valls on 11/6/24.
+//
+
+import SwiftUI
+
+struct HalfCard: View {
+    var body: some View {
+        Text("")
+        
+    }
+} // HalfCard
+
+struct DebuggingView: View {
+    var body: some View {
+        VStack {
+            HalfCard()
+            
+        } // VStack
+    }
+} // DebuggingView
+
+#Preview {
+    DebuggingView()
+}
