87af458c32a3a014a1080a6f3a4c2dbb40dd9999
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-12 19:45:47 +0200

    • f Create DiceView basic skeleton

---
+++ 03-buttons_and_state/DiceRoller/DiceRoller/DiceView.swift
@@ -0,0 +1,19 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Ignasi Perez-Valls on 12/6/24.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text("")
+        
+    }
+} // DiceView
+
+#Preview {
+    DiceView()
+}
