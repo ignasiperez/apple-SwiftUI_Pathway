5ac0cb906122fc23bffe9edd50732f46509108e1
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-12 19:39:14 +0200

    • e Create DiceRoller project

---
+++ 03-buttons_and_state/DiceRoller/DiceRoller/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Ignasi Perez-Valls on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+        
+    }
+} // ContentView
+
+#Preview {
+    ContentView()
+}
---
+++ 03-buttons_and_state/DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Ignasi Perez-Valls on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
