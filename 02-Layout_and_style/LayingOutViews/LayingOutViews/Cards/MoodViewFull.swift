138cae99a864504146dd37e99c2ad8c735e51396
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-12 14:00:16 +0200

    • f Add to the card a background within a rounded rectangle with a corner radius

--- 02-Layout_and_style/LayingOutViews/LayingOutViews/Cards/MoodViewFull.swift
+++ 02-Layout_and_style/LayingOutViews/LayingOutViews/Cards/MoodViewFull.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct MoodViewFull: View {
+    @State var value: String
+    
+    private let emojis = ["😢", "😴", "😁", "😡", "😐"]
+    
     var body: some View {
-        Text("")
-        
+        VStack {
+            Text("what's your mood?")
+                .foregroundStyle(.darkBrown)
+                .font(.headline)
+            HStack {
+                ForEach(emojis, id: \.self) { emoji in
+                    Button {
+                        value = emoji
+                    } label: {
+                        VStack {
+                            Text(emoji)
+                                .font(.system(size: 35))
+                            Image(systemName: value == emoji ? "circle.fill" : "circle")
+                                .font(.system(size: 16))
+                                .foregroundColor(.darkBrown)
+                        }
+                    }
+                }  // ForEach
+            } // HStack
+        } // VStack
+        .background(.paleOrange, in: RoundedRectangle(cornerRadius: 12))
     }
 } // MoodViewFull
 
 #Preview {
-    MoodViewFull()
+    MoodViewFull(value: "😐")
 }
