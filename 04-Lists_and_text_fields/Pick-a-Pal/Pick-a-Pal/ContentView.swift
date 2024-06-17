8e61038488e90610792079044cb0caa4b7b6653f
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-17 18:26:16 +0200

    • e Create Pick-a-Pal project

---
+++ 04-Lists_and_text_fields/Pick-a-Pal/Pick-a-Pal/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Ignasi Perez-Valls on 17/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+        
+    }
+} // ContentView
+
+#Preview {
+    ContentView()
+}
---
+++ 04-Lists_and_text_fields/Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Ignasi Perez-Valls on 17/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
