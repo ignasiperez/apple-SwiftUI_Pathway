3621fac13f25dca4b44d9f9b9c1e332133bf531c
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-18 14:23:35 +0200

    • f Define Landmark structure with a few properties matching names of landmarkData.json

--- 04-Lists_and_text_fields/Landmarks/Landmarks/Model/Landmark.swift
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Model/Landmark.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+} // Landmark
