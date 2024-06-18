3e4e2ac58c2af62956f7212c2bac329841355ca9
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-18 14:47:32 +0200

    • f Add locationCoordinate computed property useful for interacting with MapKit framework

--- 04-Lists_and_text_fields/Landmarks/Landmarks/Model/Landmark.swift
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Model/Landmark.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -22,7 +23,12 @@ struct Landmark: Hashable, Codable {
     }
     
     private var coordinates: Coordinates
-
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
 
     struct Coordinates: Hashable, Codable {
         var latitude: Double
