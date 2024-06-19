d98ca6f26d93ea13fe2d55158419947f7e6b2043
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-19 13:06:39 +0200

    • f Create a new SwiftUI view in the Views group named LandmarkRow.swift

---
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 19/6/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text("")
+    }
+} // LandmarkRow
+
+#Preview {
+    LandmarkRow()
+}
