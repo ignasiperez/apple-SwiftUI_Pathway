c6127d78302ff9723659bd445ed4007480e6cd85
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-19 14:02:27 +0200

    • f Create a new SwiftUI view in the Screens group named LandmarkList.swift

---
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 19/6/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+} // LandmarkList
+
+#Preview {
+    LandmarkList()
+}
