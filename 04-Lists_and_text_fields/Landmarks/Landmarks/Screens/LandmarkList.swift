12bdbaeeecde975535c1a4b40fa7653a9d271b2a
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-20 14:26:30 +0200

    • f Remove the id parameter in the List initializaer

--- 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks, id: \.id) { landmark in
+        List(landmarks) { landmark in
             LandmarkRow(landmark: landmark)
         }
         
