4b84723d6102bb6c1af96bbd831f3cd0488c10ef
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-21 10:23:10 +0200

    • r Change ContentView to instead display LandmarkList

--- 04-Lists_and_text_fields/Landmarks/Landmarks/ContentView.swift
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/ContentView.swift
@@ -1,45 +1,16 @@
 /*
-See the LICENSE.txt file for this sample’s licensing information.
-
-Abstract:
-A view showing the details for a landmark.
-*/
+ See the LICENSE.txt file for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing the details for a landmark.
+ */
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-
-                Divider()
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-
-            Spacer()
-            
-        } // VStack
+        LandmarkList()
+        
     }
 } // ContentView
 
