bd5b3098f5c4e4d61fc6103b7d433d4ffff0a46a
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-21 10:18:13 +0200

    • f Create a new SwiftUI view in the Screens group named LandmarkDetail.swift

---
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 21/6/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+} // LandmarkDetail
+
+#Preview {
+    LandmarkDetail()
+}
