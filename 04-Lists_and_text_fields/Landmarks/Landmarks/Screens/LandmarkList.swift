65a4f8a858afbfc4ce1a4e8ef03183c327071b62
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-22 06:34:38 +0200

    • f Add a NavigationLink nside the list’s closure specifying the LandmarkDetail view as the destination

--- 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
@@ -12,7 +12,11 @@ struct LandmarkList: View {
     var body: some View {
         NavigationSplitView {
             List(landmarks) { landmark in
-                LandmarkRow(landmark: landmark)
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
             }
             .navigationTitle("Landmarks")
         } detail: {
