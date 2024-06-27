94bf50829022014f9036c67a7bd5dcd7a936a6a5
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-06-27 12:29:47 +0200

    • f Create a nested ForEach group to transform the landmarks into rows

--- 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Screens/LandmarkList.swift
@@ -19,18 +19,20 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List { 
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
         } // NavigationSplitView
-
+        
     }
 } // LandmarkList
 
