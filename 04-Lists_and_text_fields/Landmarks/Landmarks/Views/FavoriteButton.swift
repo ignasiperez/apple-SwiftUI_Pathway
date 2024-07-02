c884fd7af39841e46fc909510e0e1c5927291dcc
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-07-02 14:15:37 +0200

    • f Create a new view called FavoriteButton.swift

---
+++ 04-Lists_and_text_fields/Landmarks/Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 2/7/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+} // FavoriteButton
+
+#Preview {
+    FavoriteButton()
+}
